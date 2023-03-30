aade5d86d3d68c73fd0c79dc4947a803f58028b4
amarrero86mr <amarrero86mr@gmail.com>
2023-03-30 14:10:39 -0300

    Agregando Lista de tareas

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,29 @@
+import { Task } from "./Task"
+
+export const TASKS: Task[] = [
+    
+    {
+        id: 1,
+        text: "Terminar el primer modulo Angular",
+        day: "agosto 5 - 12:00",
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "Hacer compras para la cena",
+        day: "agosto 5 - 18:00",
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "Estudiar sobre Boostrap y Flex",
+        day: "agosto 5 - 19:00",
+        reminder: false
+    },
+    {
+        id: 1,
+        text: "Ver siguiente pelicula de x-men",
+        day: "agosto 5 - 21:30",
+        reminder: false
+    },
+]
